542fb0d399844e13d54d08f150b967450fa80e19
Ahmet Tunç Aras <74108753+Latth@users.noreply.github.com>
2022-03-09 17:47:58 +0300

    education page created

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,25 @@
 // navbar toggle
 
 let navBtn = document.querySelector('.nav-burger-btn');
+let navIcon = document.querySelector('.nav-icon');
 
 navBtn.addEventListener('click', function(e) {
     e.preventDefault();
     let nav = document.querySelector('.nav-burger');
     nav.classList.toggle('active');
     navBtn.classList.toggle('active')
+
+    console.log(navIcon);
+
+    if(navIcon.className == "fa-solid fa-bars nav-icon"){
+        setTimeout(function(){
+            navIcon.className = "fa-solid fa-x nav-icon";
+        }, 300)
+    }else {
+        setTimeout(function(){
+            navIcon.className = "fa-solid fa-bars nav-icon"
+        }, 300)
+    }
 })
 
 // Scroll screen navbar background color
